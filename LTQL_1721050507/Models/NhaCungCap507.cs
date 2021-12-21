0443d10189c64bdf828cd282e7014feb7591f17a
PhamDucThuan <myemail>
2021-12-21 13:12:46 +0700

    Create_Table_NCC_PDT507

---
+++ LTQL_1721050507/Models/NhaCungCap507.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace LTQL_1721050507.Models
+{
+    public class NhaCungCap507
+    {
+        [Key]
+        public int MaNhaCungCap { get; set; }
+        [StringLength(50)]
+        public int TenNhaCungCap { get; set; }
+    }
+}
